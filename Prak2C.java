2ad786f689f3f9a90776650728d07ddbd5dd580b
uzl <fauzirahmaan21fb@gmail.com>
2021-09-14 21:37:08 +0700

    Praktikum 2 B

---
+++ Prak2C.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Prak2C {
+    public static void main(String[] args) {
+        
+    Scanner Beo = new Scanner(System.in);
+    String beo;
+    beo = Beo.nextLine();
+
+    System.out.println(beo);
+    }
+}
