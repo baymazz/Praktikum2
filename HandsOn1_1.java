067386d6f0572a360e140e4e30a745f28329f29c
uzl <fauzirahmaan21fb@gmail.com>
2021-09-21 12:10:22 +0700

    Tugas Praktikum

---
+++ HandsOn1_1.java
@@ -0,0 +1,10 @@
+public class HandsOn1_1 {
+    public static void main(String[] args) {
+        String s = "Java is Great Fun!";
+        System.out.println(s);
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.charAt(2));
+        System.out.println(s.length());
+    }
+}
